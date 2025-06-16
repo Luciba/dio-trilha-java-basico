4370c04d4da9f78e28934cc4d7996ec014005deb
Luciba <luciano.srjunior@gmail.com>
2025-06-15 22:41:42 -0300

    Projeto Processo Seletivo

---
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,41 @@
+package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        selecaoCandidatos();
+    }
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou um salario de " + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("Candidato " + candidato + " selecionado!");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato!");
+        } else if (salarioBase == salarioPretendido)
+            System.out.println("Ligar para o candidato com contra proposta");
+        else {
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+        
+    }
+}   
